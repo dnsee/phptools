169ca12608ff15c7eed4b04506fc2e578aff1b21
David Funaro <ing.davidino@gmail.com>
2013-08-13 18:09:57 +0200

    check folder exist

--- setSymfonyPermission.sh
+++ setSymfonyPermission.sh
@@ -1,4 +1,14 @@
 #/bin/bash
+CACHE="app/cache"
+LOGS="app/logs"
+
+if [ ! -d "$CACHE" ]; then
+	mkdir -p "$CACHE"
+fi
+
+if [ ! -d "$LOGS" ]; then
+	mkdir "$LOGS"
+fi
 
 APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd' | grep -v root | head -1 | cut -d\  -f1`;
 sudo chmod +a "$APACHEUSER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs;
